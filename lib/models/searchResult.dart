415b2d93266563b664f36b4a456c58dd2c1e2e85
Manish <manish2003sharma@gmail.com>
2025-10-27 00:24:45 +0530

    models

--- lib/models/searchResult.dart
+++ lib/models/searchResult.dart
@@ -1,4 +1,31 @@
 class SearchResult {
-  // implement fields if you have API schema
-  SearchResult();
+  final int profileId;
+  final String name;
+  final String age;
+  final String gotra;
+  final String education;
+  final String incomeCategory;
+  final String defaultPhoto;
+
+  SearchResult({
+    required this.profileId,
+    required this.name,
+    required this.age,
+    required this.gotra,
+    required this.education,
+    required this.incomeCategory,
+    required this.defaultPhoto,
+  });
+
+  factory SearchResult.fromJson(Map<String, dynamic> json) {
+    return SearchResult(
+      profileId: json['ProfileId'] ?? 0,
+      name: json['Name'] ?? '',
+      age: json['Age'] ?? '',
+      gotra: json['Gotra'] ?? '',
+      education: json['Education'] ?? '',
+      incomeCategory: json['IncomeCategory'] ?? '',
+      defaultPhoto: json['DefaultPhoto'] ?? '',
+    );
+  }
 }
