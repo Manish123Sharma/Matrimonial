e0511032358134ce69ab430a65c6fa78ede6cc59
Manish <manish2003sharma@gmail.com>
2025-10-29 14:48:43 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:matrimonial/blocs/auth_bloc/auth_bloc.dart';
-import 'package:matrimonial/screens/login_screen.dart';
-import 'package:matrimonial/screens/search_view.dart';
-import 'package:matrimonial/services/api_service.dart';
+import 'package:go_router/go_router.dart';
+import 'package:matrimonial/blocs/auth_bloc/auth_state.dart';
+import 'blocs/auth_bloc/auth_bloc.dart';
+import 'services/api_service.dart';
+import 'router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize ApiService and cookie jar (persist: true) to check login state.
   final apiService = ApiService();
   await apiService.init(persistCookies: true);
 
-  // Check cookies for base domain
-  final cookies = await apiService.cookieJar?.loadForRequest(
-    Uri.parse(ApiService.baseUri),
-  );
-
+  final cookies = await apiService.cookieJar?.loadForRequest(Uri.parse(ApiService.baseUri));
   final bool isLoggedIn = cookies != null && cookies.isNotEmpty;
 
-  runApp(MyApp(isLoggedIn: isLoggedIn, apiService: apiService));
+  final authBloc = AuthBloc(apiService: apiService);
+
+  // Emit AuthSuccess if cookies exist
+  if (isLoggedIn) {
+    authBloc.emit(AuthSuccess());
+  }
+
+  final appRouter = AppRouter(apiService: apiService, authBloc: authBloc);
+
+  runApp(MyApp(authBloc: authBloc, appRouter: appRouter));
 }
 
 class MyApp extends StatelessWidget {
-  final bool isLoggedIn;
-  final ApiService apiService;
-  const MyApp({super.key, required this.isLoggedIn, required this.apiService});
+  final AuthBloc authBloc;
+  final AppRouter appRouter;
+
+  const MyApp({super.key, required this.authBloc, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        // Provide AuthBloc app-wide
-        BlocProvider<AuthBloc>(
-          create: (_) => AuthBloc(apiService: apiService),
-        ),
+        BlocProvider<AuthBloc>.value(value: authBloc),
       ],
-      child: MaterialApp(
+      child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
         title: 'Maheshwari Matrimonial',
         theme: ThemeData(primarySwatch: Colors.red),
-        home: isLoggedIn ? const SearchScreen() : const LoginScreen(),
+        routerConfig: appRouter.router,
       ),
     );
   }
