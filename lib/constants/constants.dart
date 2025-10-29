3323e7f5729da28037664fe1f88374968946df84
Manish <manish2003sharma@gmail.com>
2025-10-29 14:03:42 +0530

    constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const baseUrl = 'https://test.maheshwari.org';
+  static const authEndpoint = '/api/user/authenticate';
+  static const searchEndpoint = '/api/search/get-search-result';
+}
